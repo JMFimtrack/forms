bf5d4cac827d414c3852e1b37c479f624b3d6217
Valentin <jesus.martinez@fimtrack.com.mx>
2025-01-28 16:51:38 -0600

    Auto-generated commit

---
+++ chunk-5PXXLYLO.js
@@ -0,0 +1,1 @@
+import{Bb as p,Sa as n,lb as t,mb as r}from"./chunk-FZWCJOO2.js";var s=class e{static \u0275fac=function(o){return new(o||e)};static \u0275cmp=n({type:e,selectors:[["app-error-not-found"]],decls:2,vars:0,template:function(o,i){o&1&&(t(0,"p"),p(1,"error-not-found works!"),r())},encapsulation:2})};export{s as ErrorNotFoundComponent};

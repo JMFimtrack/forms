37d28bb6c592ba89bcbb82d70d8f040d06ea3803
Valentin <jesus.martinez@fimtrack.com.mx>
2025-01-28 11:39:17 -0600

    Feat(FrontEnd): Feat Input
    Feat input base component

--- src/app/components/input-text/input-text.component.ts
+++ src/app/components/input-text/input-text.component.ts
@@ -22,12 +22,22 @@ export class InputTextComponent {
   protected dataForm = inject(DataFormService);
   protected readonly value = signal('');
 
-  protected nameFormControl = new FormControl(this.value(), [Validators.required, Validators.pattern('[a-zA-Z ]*')]);
+  protected nameFormControl = new FormControl(
+    this.value(),
+    [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      Validators.pattern('[a-zA-Z ]*')
+    ]
+  );
 
   protected onInput(event: Event) {
     this.value.set((event.target as HTMLInputElement).value);
 
     this.dataForm.setData(this.nameFormControl.value || '');
     console.log('this', this.dataForm.getData());
+    console.log(this.nameFormControl.errors);
+
   }
 }
